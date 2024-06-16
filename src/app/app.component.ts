025b9577cb1ed1553c8bff6ca311eaf030e0c036
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-06-16 16:45:51 -0700

    Learning how to use primeng on Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, RouterOutlet
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: []
 })
 export class AppComponent {
-  title = 'practice-project';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: GalleryComponent },
+    { path: 'second', component: SecondComponent }
+];
