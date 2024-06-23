8110651f91999d6be2714d3e2895c2886ad86561
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-06-23 16:22:50 -0700

    Working around with canva-confetti and PrimeNG

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GalleriaModule } from 'primeng/galleria';
 import { ImagesService } from '../../services/images.service';
 import { ButtonModule } from 'primeng/button';
+import * as confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-gallery',
@@ -15,6 +16,8 @@ import { ButtonModule } from 'primeng/button';
 export class GalleryComponent implements OnInit {
 
   images: any[] | undefined;
+  heartsArray: string[] = [];
+  changeColor = false;
 
   responsiveOptions: any[] = [
     {
@@ -31,10 +34,51 @@ export class GalleryComponent implements OnInit {
     }
 ];
 
-  constructor(private imgService: ImagesService){}
+  constructor(
+    private imgService: ImagesService,
+    private renderer2: Renderer2,
+    private elementRef: ElementRef
+  ){}
 
   ngOnInit(): void {
       this.images = this.imgService.getImages();
+      this.heartsArray = new Array(1215).fill('❤️');
+  }
+
+  makeRainConfetti() {
+    let canvas = this.renderer2.createElement('canvas');
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    this.renderer2.appendChild(document.body, canvas);
+    const myConfetti = confetti.create(canvas, {
+      resize: true
+    })
+    myConfetti({
+      particleCount: 1000,
+      startVelocity: 30,
+      spread: 360,
+      origin: {
+        x: 0.5,
+        y: 0
+      }
+    })?.then(() => {
+      this.renderer2.removeChild(document.body, canvas);
+    });
+
+  }
+
+  changeHeartsColor() {
+    let heartsElement = document.querySelectorAll('.hearts');
+    heartsElement.forEach((heart) => {
+      if(this.changeColor) {
+        (heart as HTMLElement).style.color = 'tomato';
+      } else  {
+        (heart as HTMLElement).style.color = 'blueviolet';
+      }
+    });
+
+    this.changeColor = !this.changeColor;
   }
 
 }
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { SidebarModule } from 'primeng/sidebar';
+
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [CardModule, ButtonModule, RouterLink],
+  imports: [
+    CardModule, 
+    ButtonModule, 
+    SidebarModule, 
+    RouterLink, 
+  ],
   templateUrl: './second.component.html',
   styleUrl: './second.component.css'
 })
 export class SecondComponent {
+  sidebarVisible1 : boolean = false;
 
 }
